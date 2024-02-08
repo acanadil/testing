a125dab21ef38b3452eb764ce9c7eb12d92619b0
acando <albert.canadilla@estudiants.urv.cat>
2024-02-08 10:51:47 +0100

    added useless module and syntax error

--- predictor/test_predictor.py
+++ predictor/test_predictor.py
@@ -5,4 +5,4 @@ def test_predict_age():
     assert 0 <= Predictor.predict_age(0,7) <= 7
     assert -15.0 <= Predictor.predict_age(-15.0,5.0) <= 5.0
     assert Predictor.predict_age(1,1) == 1
-    assert Predictor.predict_age("a", 35)
+    #assert Predictor.predict_age("a", 35)
