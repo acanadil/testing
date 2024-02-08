3761fd6fe9912b06b288ad70adcf5c1f8e5d0569
acando <albert.canadilla@estudiants.urv.cat>
2024-02-08 10:11:08 +0100

    commit

---
+++ predictor/predictor.py
@@ -0,0 +1,8 @@
+import random
+
+class Predictor:
+
+    def predict_age(min_age, max_age):
+        age = random.randint(int(min_age), int(max_age))
+        print("Your age is.. " + str(age) + "!")
+        return age
